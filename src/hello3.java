4b9588ee3a9f49c56909523486876aea371d6edf
amarnath113 <57338834+amarnath113@users.noreply.github.com>
2019-11-28 21:11:25 +0530

    Update hello3.java

--- src/hello3.java
+++ src/hello3.java
@@ -2,6 +2,6 @@ class hello3
 {
     public static void main(String args[])
 	{
-	System.out.println("hello world from class3 modify...");
+	System.out.println("hello world from class5 modify...");
 	}
 	}
